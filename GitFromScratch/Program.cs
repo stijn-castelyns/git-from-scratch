1206c1c040feb719d3d45dbe2458c1774d1b2fc2
stijn-castelyns <72084313+stijn-castelyns@users.noreply.github.com>
2026-04-05 12:13:25 +0200

    Step 0: Initial code

--- GitFromScratch/Program.cs
+++ GitFromScratch/Program.cs
@@ -1,17 +1,24 @@
-﻿using GitFromScratch.Commands;
-using Spectre.Console.Cli;
+﻿using Spectre.Console.Cli;
 
 CommandApp? app = new CommandApp();
 
 app.Configure(config =>
 {
-    config.AddCommand<InitRepoCommand>("init");
-    config.AddCommand<HashObjectCommand>("hash-object");
-    config.AddCommand<AddCommand>("add");
-    config.AddCommand<CommitCommand>("commit");
-    config.AddCommand<BranchCommand>("branch");
-    config.AddCommand<CheckoutCommand>("checkout");
-    config.AddCommand<MergeCommand>("merge");
+    config.AddCommand<HelloWorldCommand>("hello");
 });
 
 return app.Run(args);
+
+public class HelloWorldCommand : Command<HelloWorldCommand.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        [CommandArgument(0, "<NAME>")]
+        public string Name { get; set; } = string.Empty;
+    }
+    public override int Execute(CommandContext context, Settings settings, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Hello, {settings.Name}!");
+        return 0;
+    }
+}
